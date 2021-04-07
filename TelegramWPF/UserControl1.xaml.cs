de34ea99db6dc46d5cd53c71126d31984395356e
Zaur Jafarov <zaurcfr@hotmail.com>
2021-04-07 16:01:09 +0400

    Finished

---
+++ TelegramWPF/UserControl1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TelegramWPF
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+        public string NameLabel
+        {
+            get { return NameLbl.Content.ToString(); }
+            set { NameLbl.Content = value; }
+        }
+        public string MessageLabel
+        {
+            get { return MessageLbl.Content.ToString(); }
+            set { MessageLbl.Content = value; }
+        }
+        public ImageSource image
+        {
+            get { return ImageBox.ImageSource; }
+            set { ImageBox.ImageSource = value; }
+        }
+    }
+}
